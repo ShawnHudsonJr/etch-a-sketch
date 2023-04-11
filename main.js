a64b6b601f41c6f7bf9c1ad10f9b1130fd3fafa9
Shawn Hudson Jr <shawnhudsonjr@gmail.com>
2023-04-11 08:27:48 -0400

    Began work on the various color functions

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 const sketchpad = document.querySelector("#sketchpad");
-//Size Adjustment
+
+let blockColor;
+const random = ['red', 'blue', 'yellow', 'green', 'brown', 'black', 'orange', 'pink', 'purple', 'white'];
+
+//Set To Colorful
+function randomColor() {
+    //Random number between 0 and 9
+    let x = (Math.floor(Math.random() * 10));
+    x = random[x];
+    return x;
+}
+
+const colorful = document.querySelector('.colorful');
+colorful.addEventListener('click', function () {
+    blockColor = randomColor();
+});
+
+// Set To Monochrome
+const monochrome = document.querySelector(".monochrome");
+monochrome.addEventListener('click', function() {
+    blockColor = 'black';
+});
+
+// Clear Sketchpad
+const clear = document.querySelector(".clear");
+clear.addEventListener('click', function() {
+    blockColor = 'white';
+});
+
+//Size Selector
 const size = document.querySelector(".size");
 size.addEventListener('click', function() {
 
+    //create grid
     let gridsize = prompt("Pick a Grid Size");
     let boxsize;
 
@@ -16,34 +46,14 @@ size.addEventListener('click', function() {
             let newRow = document.createElement("div");
             newRow.className = "row";
             newColumn.appendChild(newRow);
+
+            //Hover Effect
+            newRow.addEventListener('mouseover', function() {
+                newRow.style.backgroundColor = blockColor;
+            });
+
         }
-
     }
-/*
-    let column = document.getElementsByClassName(".column");
-    column.style.height = boxsize + "px";
-    column.style.width = boxsize + "px";
-    let row = document.getElementsByClassName(".row");
-    row.style.height = boxsize + "px";
-    row.style.width = boxsize + "px";
-    */
-});
-
-// Set To Monochrome
-const monochrome = document.querySelector(".monochrome");
-monochrome.addEventListener('click', function() {
-
-});
-
-//Set To Colorful
-const colorful = document.querySelector(".colorful");
-colorful.addEventListener('click', function() {
-
-});
-
-// Clear Sketchpad
-const clear = document.querySelector(".clear");
-clear.addEventListener('click', function() {
 
 });
 
